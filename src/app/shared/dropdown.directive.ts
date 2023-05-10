c28d5b7191df7e1c0786e6ecab1408d9b27969c3
Haitt23 <haitt23@>
2023-05-10 09:04:58 +0700

    ADD: handing click anywhere to close directive for dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,5 +1,6 @@
 import {
   Directive,
+  ElementRef,
   HostBinding,
   HostListener
 } from "@angular/core";
@@ -10,7 +11,12 @@ import {
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  // @HostListener('click') toggleOpen() {
+  //   this.isOpen = !this.isOpen;
+  // }
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
